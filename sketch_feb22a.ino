3a74adecf96287a5105b8ac55e53ec6ae6048d4f
eshaalandmoosa <100220410+eshaalandmoosa@users.noreply.github.com>
2022-02-22 20:59:27 +0500

    Add files via upload

---
+++ sketch_feb22a.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(3,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  if (digitalRead(3)== LOW)
+  {
+    digitalWrite(13,HIGH);
+    
+   
+  }
+  else 
+  {
+    
+    digitalWrite(13,LOW);
+    
+  }
+  
+}
